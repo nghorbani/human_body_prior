d28ef52026e1a9f4798c5ac018b19c993c4a0675
Nima Ghorbani <nimaqzzz@gmail.com>
2019-06-11 15:26:10 +0200

    Add early stop code

---
+++ human_body_prior/tools/training_tools.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.val_loss_min = np.Inf
+
+    def __call__(self, val_loss):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+        elif score < self.best_score:
+            self.counter += 1
+            # print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                return True
+        else:
+            self.best_score = score
+            self.counter = 0
+        return False
