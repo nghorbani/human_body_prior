d13b7120babb5c7447244b22a0de7c786241664f
Nima Ghorbani <nimaqzzz@gmail.com>
2019-10-10 18:50:22 +0200

    Make dependency on configer 1.4.1

--- setup.py
+++ setup.py
@@ -36,11 +36,12 @@ setup(name='human_body_prior',
       description='Variational human pose prior for human pose synthesis and estimation.',
       long_description=open("README.md").read(),
       long_description_content_type="text/markdown",
-      install_requires=['torch==1.1.0', 'tensorboardX>=1.6', 'torchgeometry==0.1.2', 'opencv-python>=4.1.0.25',
-                        'scikit-image>=0.15.0', 'configer', 'imageio>=2.5.0', 'transforms3d>=0.3.1', 'trimesh',
+      install_requires=['torch==1.1.0', 'tensorboardX>=1.6', 'torchgeometry==0.1.2', 'opencv-python>=4.1.0.25','configer>=1.4',
+                        # 'scikit-image>=0.15.0',
+                        'configer', 'imageio>=2.5.0', 'transforms3d>=0.3.1', 'trimesh',
                         'smplx', 'pyrender', 'moviepy'],
       dependency_links=[
-          "https://github.com/nghorbani/configer/tarball/master#egg=configer"
+          "https://github.com/nghorbani/configer/tarball/1.4.1#egg=configer-1.4.1"
       ],
       classifiers=[
           "Intended Audience :: Developers",
